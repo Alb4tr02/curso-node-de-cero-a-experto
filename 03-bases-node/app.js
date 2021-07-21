cd964fc36648abe04cfaf7000fba24ae69d58a15
Tony <834@holbertonschool.com>
2021-07-20 22:36:51 -0500

    add --listar command to show result in console

--- 03-bases-node/app.js
+++ 03-bases-node/app.js
@@ -5,10 +5,21 @@ const argv = require('yargs')
         alias: 'base',
         type: 'number',
         demandOption: true,
-    }).argv
+    })
+    .option('l', {
+        alias: 'listar',
+        type: 'boolean',
+        demandOption: true,
+        default: false,
+    })
+    .check((argv, options) => {
+        if (isNaN(argv.b))
+            throw 'La base debe ser un número';
+        return true;
+    })
+    .argv
 
 console.clear()
-console.log(argv);
-crearArchivo(argv.b)
+crearArchivo(argv.b, argv.l)
     .then(nombreArchivo => console.log(nombreArchivo, 'creado'))
     .catch(err => console.log(err));
--- 03-bases-node/utils/multiplicar.js
+++ 03-bases-node/utils/multiplicar.js
@@ -1,12 +1,14 @@
 const fs = require('fs');
 
-const crearArchivo = async (base = 5) => {
+const crearArchivo = async (base = 5, listar) => {
 
     let salida = '';
     for (let i = 1; i <= 10; i++)
         salida += `${base} x ${i} = ${base * i}\n`;
     try {
         fs.writeFileSync(`tabla-${base}.txt`, salida);
+        if (listar)
+            console.log(salida);
         return `tabla-${base}.txt`;
     } catch (error) {
         throw error;
