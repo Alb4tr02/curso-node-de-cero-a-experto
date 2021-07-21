e58ad90a70d8c77ba3db27ba9168cbacb83d9e20
Tony <834@holbertonschool.com>
2021-07-20 22:41:23 -0500

    add forlder to separate yargs config from app.js

--- 03-bases-node/app.js
+++ 03-bases-node/app.js
@@ -1,23 +1,6 @@
 const fs = require('fs');
 const { crearArchivo } = require('./utils/multiplicar.js');
-const argv = require('yargs')
-    .option('b', {
-        alias: 'base',
-        type: 'number',
-        demandOption: true,
-    })
-    .option('l', {
-        alias: 'listar',
-        type: 'boolean',
-        demandOption: true,
-        default: false,
-    })
-    .check((argv, options) => {
-        if (isNaN(argv.b))
-            throw 'La base debe ser un número';
-        return true;
-    })
-    .argv
+const argv = require('./config/yargs.js');
 
 console.clear()
 crearArchivo(argv.b, argv.l)
