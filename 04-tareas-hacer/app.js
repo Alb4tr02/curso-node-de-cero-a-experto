bbcf3683377d6d34ebcd7be27d66cd327827eee6
Tony <834@holbertonschool.com>
2021-07-21 22:01:59 -0500

    create mostrarMenu function

--- 04-tareas-hacer/app.js
+++ 04-tareas-hacer/app.js
@@ -1,9 +1,18 @@
 require('colors');
+
+const { mostrarMenu, pausa } = require('./helpers/mensajes.js');
+
 console.clear();
 
 const main = async () => {
     
     console.log('Hola mundo');
+    let input = ""
+    while (input != "0") {
+        input = await mostrarMenu();
+        await pausa();    
+    }
+    
 
 }
 
